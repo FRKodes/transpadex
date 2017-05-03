58205a39395a0bb774e3c306ef200790d772a8d3
frkodes <frkalderon@gmail.com>
2017-05-03 16:05:57 -0500

    Services done

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,8 +6,13 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    public function index()
-    {
-    	return View('pages.index');
-    }
+	public function index()
+	{
+		return View('pages.index');
+	}
+
+	public function servicios()
+	{
+		return View('pages.servicios');
+	}
 }
---
+++ resources/views/pages/servicios.blade.php
@@ -0,0 +1,142 @@
+@extends('layout')
+
+@section('content')
+	<div class="section-top-banner servicios">
+		<div class="container">
+			<p class="asap">Estamos <br>para servirte</p>
+		</div>
+	</div>
+
+	<div class="container">
+		<h1 class="asap text-center section-title italic">Fletes</h1>
+		<div class="row">		
+			<div class="col-sm-6 service-item">
+				<div class="icon-service"><img src="images/servicio-cobertura-nacional.svg" alt=""></div>
+
+				<div class="info">
+					<div class="title oswald italic">Cobertura Nacional</div>
+					<div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut repudiandae possimus magnam accusamus rerum amet minus adipisci.</div>
+				</div>
+			</div>
+			<div class="col-sm-6 service-item">
+				<div class="icon-service"><img src="images/servicio-door-to-door.svg" alt=""></div>
+
+				<div class="info">
+					<div class="title oswald italic">Recolección door to door</div>
+					<div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut repudiandae possimus magnam accusamus rerum amet minus adipisci.</div>
+				</div>
+			</div>
+			<div class="col-sm-6 service-item">
+				<div class="icon-service"><img src="images/servicio-asesoria-manejo.svg" alt=""></div>
+
+				<div class="info">
+					<div class="title oswald italic">Asesoría en el Manejo</div>
+					<div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut repudiandae possimus magnam accusamus rerum amet minus adipisci.</div>
+				</div>
+			</div>
+			<div class="col-sm-6 service-item">
+				<div class="icon-service"><img src="images/servicio-seguridad.svg" alt=""></div>
+
+				<div class="info">
+					<div class="title oswald italic">Seguro de carga opcional</div>
+					<div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut repudiandae possimus magnam accusamus rerum amet minus adipisci.</div>
+				</div>
+			</div>
+			<div class="col-sm-6 service-item">
+				<div class="icon-service"><img src="images/servicio-por-cobrar.svg" alt=""></div>
+
+				<div class="info">
+					<div class="title oswald italic">Servicio por Cobrar</div>
+					<div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut repudiandae possimus magnam accusamus rerum amet minus adipisci.</div>
+				</div>
+			</div>
+			<div class="col-sm-6 service-item">
+				<div class="icon-service"><img src="images/servicio-maniobras-carga-descarga.svg" alt=""></div>
+
+				<div class="info">
+					<div class="title oswald italic">Maniobras de carga y descarga</div>
+					<div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut repudiandae possimus magnam accusamus rerum amet minus adipisci.</div>
+				</div>
+			</div>
+			<div class="col-sm-6 service-item">
+				<div class="icon-service"><img src="images/servicio-necesidades.svg" alt=""></div>
+
+				<div class="info">
+					<div class="title oswald italic">Desarrollos según la necesidad</div>
+					<div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut repudiandae possimus magnam accusamus rerum amet minus adipisci.</div>
+				</div>
+			</div>
+			<div class="col-sm-6 service-item">
+				<div class="icon-service"><img src="images/servicio-rastreo-satelital.svg" alt=""></div>
+
+				<div class="info">
+					<div class="title oswald italic">Rastreo satelital</div>
+					<div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut repudiandae possimus magnam accusamus rerum amet minus adipisci.</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid">
+		<div class="row storage-services">
+			<div class="container">
+				<h2 class="text-center oswald mayus section-title m-b-40">Almacenamiento y distribución</h2>
+				<div class="col-sm-6 storage-item">
+					<div class="item-container">
+						<span class="icon-check"></span>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+					</div>
+				</div>
+
+				<div class="col-sm-6 storage-item">
+					<div class="item-container">
+						<span class="icon-check"></span>
+						<p>Amet, consectetur adipisicing.</p>
+					</div>
+				</div>
+
+				<div class="col-sm-6 storage-item">
+					<div class="item-container">
+						<span class="icon-check"></span>
+						<p>Ipsum dolor sit amet, consectetur adipisicing elit.</p>
+					</div>
+				</div>
+
+				<div class="col-sm-6 storage-item">
+					<div class="item-container">
+						<span class="icon-check"></span>
+						<p>Dolor sit amet, consectetur adipisicing.</p>
+					</div>
+				</div>
+
+				<div class="col-sm-6 storage-item">
+					<div class="item-container">
+						<span class="icon-check"></span>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+					</div>
+				</div>
+
+				<div class="col-sm-6 storage-item">
+					<div class="item-container">
+						<span class="icon-check"></span>
+						<p>Amet, consectetur adipisicing.</p>
+					</div>
+				</div>
+
+				<div class="col-sm-6 storage-item">
+					<div class="item-container">
+						<span class="icon-check"></span>
+						<p>Ipsum dolor sit amet, consectetur adipisicing elit.</p>
+					</div>
+				</div>
+
+				<div class="col-sm-6 storage-item">
+					<div class="item-container">
+						<span class="icon-check"></span>
+						<p>Dolor sit amet, consectetur adipisicing.</p>
+					</div>
+				</div>
+			</div>
+		</div>	
+	</div>
+@stop
