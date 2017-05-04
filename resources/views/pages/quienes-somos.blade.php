e135fd2a99033bfb3d39039a97d1996658fd7582
frkodes <frkalderon@gmail.com>
2017-05-04 16:11:14 -0500

    who we are, done

--- resources/views/pages/quienes-somos.blade.php
+++ resources/views/pages/quienes-somos.blade.php
@@ -15,44 +15,69 @@
 	</div>
 
 	<div class="container">
-		<div class="col-sm-3 history-item first-one">
+		<div class="col-sm-4 history-item first-one">
 			<div class="title">1991</div>
 			<div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto sit facere neque.</div>
-			<span class="icon-flecha-abajo red hidden-sm"></span>
+			<span class="icon-flecha-abajo red hidden-sm hidden-md hidden-lg"></span>
+		</div>
+		<div class="col-sm-1 arrow hidden-xs">
+			<span class="icon-flecha-der red _768"></span>
 		</div>
 
-		<div class="col-sm-3 history-item second-one">
+		<div class="col-sm-4 history-item second-one">
 			<div class="title">2014</div>
 			<div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto sit facere neque.</div>
-			<span class="icon-flecha-abajo red hidden-sm"></span>
+			<span class="icon-flecha-abajo red hidden-sm hidden-md hidden-lg"></span>
+		</div>
+		<div class="col-sm-1 arrow hidden-xs">
+			<span class="icon-flecha-der red _768"></span>
 		</div>
 	</div>
 
 	<div class="container">
-		<div class="item mision">
-			<div class="title oswald italic">Misión</div>
-			<div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas quia tenetur vel possimus distinctio maxime id non neque sapiente vitae unde deleniti sint ea architecto fugit.</div>
+		<div class="item mision row">
+			<div class="image-item col-sm-3 col-md-4 hidden-xs">
+				<img src="images/quienes-somos-mision.jpg" alt="quienes somos mision">
+			</div>
+			<div class="col-sm-9 col-md-8">
+				<div class="title oswald italic">Misión</div>
+				<div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas quia tenetur vel possimus distinctio maxime id non neque sapiente vitae unde deleniti sint ea architecto fugit.</div>
+			</div>
 		</div>
 
-		<div class="item vision">
-			<div class="title oswald italic">Visión</div>
-			<div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas quia tenetur vel possimus distinctio maxime id non neque sapiente vitae unde deleniti sint ea architecto fugit.</div>
+		<div class="item vision row">
+			<div class="image-item col-sm-3 col-md-4 hidden-xs">
+				<img src="images/quienes-somos-vision.jpg" alt="quienes somos vision">
+			</div>
+			<div class="col-sm-9 col-md-8">
+				<div class="title oswald italic">Visión</div>
+				<div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas quia tenetur vel possimus distinctio maxime id non neque sapiente vitae unde deleniti sint ea architecto fugit.</div>
+			</div>
 		</div>
 	</div>
 
-	<div class="container-fluid">
+	<div class="container-fluid valores-back">
 		<div class="row">
 			<div class="container">
 				<div class="item valores">
-					<div class="title oswald italic bg-triangle">Valores</div>
-					<div class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas quia tenetur vel possimus distinctio maxime id non neque sapiente vitae unde deleniti sint ea architecto fugit.</div>
+					<div class="title oswald italic bg-triangle col-xs-12">Valores</div>
+					<div class="items col-sm-5 col-md-4">
+						<div class="value">- Lorem ipsum dolor sit amet, consectetur.</div>
+						<div class="value">- Lorem ipsum dolor sit amet, consectetur.</div>
+						<div class="value">- Lorem ipsum dolor sit amet, consectetur.</div>
+						<div class="value">- Lorem ipsum dolor sit amet, consectetur.</div>
+						<div class="value">- Lorem ipsum dolor sit amet, consectetur.</div>
+					</div>
+					<div class="description col-sm-7 col-md-8">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet perferendis alias nesciunt illo sunt quae, deserunt laboriosam eius molestiae molestias! Quo maiores temporibus nobis omnis eligendi possimus eos quod voluptatem!
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 
 	<div class="container">
-		<div class="item advantges">
+		<div class="item advantages">
 			<div class="title oswald italic bg-triangle">Ventajas Competitivas</div>
 			<div class="item-advantage m-t-20">
 				<span class="icon-check"></span>
