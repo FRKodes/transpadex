34fb057c43dd5e708b166c6e33a73b6e7785958b
frkodes <frkalderon@gmail.com>
2017-06-07 11:25:09 -0500

    Meta keywords updated

--- resources/views/partials/metas.blade.php
+++ resources/views/partials/metas.blade.php
@@ -2,6 +2,8 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="Servicio de transporte express a toda la república, así como almacenamiento y distribución. Recolectamos donde nuestro cliente nos indique y lo trasladamos a cualquier punto de la república. Contamos con Servicio por Cobrar, rastreo satelital, Desarrollos según la necesidad, seguro de carga opcional. ¡Llámanos! 01 800 347 60 36">
-<meta name="keywords" content="">
+<meta name="keywords" content="fletes express, servicio de carga 1 tonelada, servicio de carga 3 tonelada, servicio de carga 5 tonelada, servicio de carga 10 toneladas, servicio transporte caja seca, almacenamiento y distribución, rastreo satelital, servicio por cobrar, cobertura nacional, recolección door to door, seguro de carga opcional, control de inventarios, cross dock, re empaque y etiquetado de producto, distribución local y nacional, surtido y armado de pedidos, sistema de seguridad 24/7, carga y descarga de unidades, unidades de caja seca y plataforma, servicio de carga completa (FTL), servicio de carga consolidada (LTL)">
+<meta content="all" name="robots">
+<meta content="7 days" name="revisit-after">
 <title>Transpadex - @yield('title_description', 'Su aliado en transporte dedicado.')</title>
 <link href="/css/app.css" rel="stylesheet" type="text/css">
